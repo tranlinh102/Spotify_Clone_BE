1eda9d34b276d49cd80bed59ad333d658be50e7e
tranlinh102 <linhtranvo2003@gmail.com>
2025-04-20 17:08:06 +0700

    thêm func get danh sách nhạc

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from music.views import SongUploadView
+from .views import SongListView
 
 urlpatterns = [
     path('add-song/', SongUploadView.as_view(), name='SongUploadView'),
+    path('songs/', SongListView.as_view(), name='song-list'),
 ]
--- music/views.py
+++ music/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListAPIView
 from music.models import Songs
 from music.serializers import SongsSerializer
 
@@ -17,3 +18,7 @@ class SongUploadView(APIView):
                 "song": serializer.data
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongListView(ListAPIView):
+    queryset = Songs.objects.all()
+    serializer_class = SongsSerializer
